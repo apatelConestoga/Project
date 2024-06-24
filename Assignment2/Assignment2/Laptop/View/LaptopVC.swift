45f0cd958861d491977af006350755d4a83f5113
apatelConestoga <apatel1509@conestogac.on.ca>
2024-06-23 20:29:37 -0400

    update condition in assignment   project

--- Assignment2/Assignment2/Laptop/View/LaptopVC.swift
+++ Assignment2/Assignment2/Laptop/View/LaptopVC.swift
@@ -27,18 +27,20 @@ class LaptopVC: UIViewController {
     }
     
     private func configureNavigation() {
+        //Customized navigation
         self.title = "Laptop"
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Futura-Medium", size: 20)!, NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0, green: 0.3568627451, blue: 0.2549019608, alpha: 1)]
         self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Futura-Medium", size: 30)!, NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0, green: 0.3568627451, blue: 0.2549019608, alpha: 1)]
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.largeTitleDisplayMode = .automatic
         self.navigationController?.navigationBar.sizeToFit()
-        
+        //Add right navigation button
         let plusImage = UIImage(named: "plus")?.withRenderingMode(.alwaysOriginal)
         navigationController?.navigationBar.backIndicatorImage = plusImage
         navigationController?.navigationBar.backIndicatorTransitionMaskImage = plusImage
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: plusImage, style: .plain, target: self, action: #selector(addItemsClicked))
         
+        //Add left navigation button
         let backImage = UIImage(named: "back-button")?.withRenderingMode(.alwaysOriginal)
         navigationController?.navigationBar.backIndicatorImage = backImage
         navigationController?.navigationBar.backIndicatorTransitionMaskImage = backImage
@@ -46,25 +48,32 @@ class LaptopVC: UIViewController {
 
     }
     
+    //Customized table view
     private func configureTableView() {
         self.tblLaptop.delegate = self
         self.tblLaptop.dataSource = self
         self.tblLaptop.contentInsetAdjustmentBehavior = .never
+        
+        //Add static values
         self.arrLaptopList.append(LaptopModel(title: "Dell", details: "2.7Ghz", price: 1299.99, isStatic: false))
         self.arrLaptopList.append(LaptopModel(title: "Apple", details: "2Ghz", price: 2299.99, isStatic: false))
         self.arrLaptopList.append(LaptopModel(title: "HP", details: "2.3Ghz", price: 299.99, isStatic: false))
+        self.addIBMLaptopIfNecessary()
     }
     
     
+    //Show alert
     func showAlert() {
         self.showAlertWithInputDialog(title: "Add New Item", subtitle: "", actionTitle: "Ok", cancelTitle: "Cancel") { cancel in
         } actionHandler: { objLaptop, success in
             if success {
                 if let objLaptop = objLaptop {
+                    //append user values in laptop array
                     self.arrLaptopList.append(objLaptop)
                     self.addIBMLaptopIfNecessary()
                 }
             } else {
+                //Show error alert
                 self.showSimpleAlert(title: "Please enter all details", actionTitle: "Close", alertStyle: .actionSheet) {
                     self.showAlert()
                 }
@@ -79,34 +88,43 @@ class LaptopVC: UIViewController {
             return
         }
         
-        let index = self.arrLaptopList.firstIndex(where: { $0.isStatic == true})
+        // Determine the index of the first static laptop in the array
+        let index = self.arrLaptopList.firstIndex(where: { $0.isStatic == true })
+        
+        // Only insert if there's no static laptop already in the array
         if index == nil {
-            if fifthDigit < self.arrLaptopList.count {
-                self.arrLaptopList.insert(LaptopModel(title: "IBM Tablet (#8981509)", details: "", price: 999.99, isStatic: true), at: fifthDigit)
+            if fifthDigit <= self.arrLaptopList.count {
+                let insertPosition = (fifthDigit == 0) ? fifthDigit : fifthDigit - 1
+                self.arrLaptopList.insert(LaptopModel(title: "IBM Tablet (#\(studentNumber))", details: "", price: 999.99, isStatic: true), at: insertPosition)
             }
         }
+        
+        
     }
 }
 
 //MARK: - Button Action
 extension LaptopVC {
-    
+    //Back action
     @objc func back() {
         self.navigationController?.popViewController(animated:true)
     }
     
+    //Add new item action
     @objc func addItemsClicked() {
         self.showAlert()
     }
 }
 
 extension LaptopVC: UIScrollViewDelegate {
+    //Scroll event
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView == self.tblLaptop{
+            //Scroll greater than 3 the hide large title
             if self.tblLaptop.contentOffset.y > 3.0 {
                 self.navigationController?.navigationBar.prefersLargeTitles = false
             }
-            
+            //Scroll equal to 0 the show large title 
             if self.tblLaptop.contentOffset.y == 0.0 {
                 self.navigationController?.navigationBar.prefersLargeTitles = true
                 self.navigationItem.largeTitleDisplayMode = .automatic
